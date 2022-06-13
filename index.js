0b5ce20e7989265632183b797705e0abb8a7edb5
pertinent23 <10franckpertinent@gmail.com>
2022-06-13 06:53:33 +0100

    after initialisation of the terminal

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log( 'ici' );
+const terminal = require( './terminal' );
+const lines = [];
+const used = process.argv[ 2 ] || 'exo7';
+
+terminal.prompt();
+
+terminal.on( 'line', ( data ) => {
+    let n = 0;
+    if ( lines.length ) {
+        /** 
+            *
+            * the first line give the 
+            * number of test case 
+        */
+        n = parseInt( lines[ 0 ] );
+    }
+} )
