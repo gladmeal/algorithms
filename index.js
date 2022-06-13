ea041a25146ce5fe5e8c0d69c57d700f7d7f2f8e
pertinent23 <10franckpertinent@gmail.com>
2022-06-13 09:25:15 +0100

    after adding exo 7

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const terminal = require( './terminal' );
 const lines = [];
 const used = process.argv[ 2 ] || 'exo7';
 
+const exo7 = require( './utils/exo7' );
+const exo8 = require( './utils/exo8' );
+const exo9 = require( './utils/exo9' );
+
+console.log( 'Enter your data' );
+
 terminal.prompt();
 
 terminal.on( 'line', ( data ) => {
     let n = 0;
+
     if ( lines.length ) {
         /** 
             *
@@ -13,5 +20,46 @@ terminal.on( 'line', ( data ) => {
             * number of test case 
         */
         n = parseInt( lines[ 0 ] );
+        lines.push( data.slice( 2 ) );
+    } else {
+        lines.push( data );
     }
-} )
+
+    const 
+        needed = lines.length - 1;
+    /** 
+        *
+        * here we have to manage when
+        * the terminal will stop to receive 
+        * terminal data 
+    */
+
+    if ( needed > 0 ) {
+        if ( used === 'exo7' && needed === n ) {
+                terminal.close();
+            return;
+        } else if ( used === 'exo8' && needed * 2 === n ) {
+                terminal.close();
+            return;
+        } else if ( used === 'exo9' ) {
+
+        } 
+    }
+    terminal.write( '> ' );
+} );
+
+terminal.on( 'close', () =>  {
+    switch( used ) {
+        case 'exo7':
+                exo7.call( {}, lines.slice( 1 ) );
+            break;
+    
+        case 'exo8':
+                exo8.call( {}, lines.slice( 1 ) );
+            break;
+
+        case 'exo9':
+                exo9.call( {}, lines.slice( 1 ) );
+            break;
+    }
+} );
--- utils/exo7.js
+++ utils/exo7.js
@@ -1,3 +1,81 @@
-module.exports = function () {
+function separateData( line ) {  
+    return line.split( ' ' );
+};
+
+function createTuple( arr = [] ) {
+    const 
+        tuple = {
+            x: [],
+            y: []
+        };
     
+        for( let i = 0; i < arr.length; i++ ) {
+            let j =  parseInt( arr[ i ].trim() || '0' );
+            if ( j % 2 === 0 ) {
+                tuple.y.push( j );
+            } else {
+                tuple.x.push( j );
+            }
+        }
+    return tuple;
+};
+
+function init( line ) {
+    let temp, needed;
+    const list = [];
+    const 
+        part = separateData( line );
+            for( let i = 0; i < part.length; i++ ) {
+                temp = part[ 0 ];
+                for ( let j = 1; j < part.length; j++ ) {
+                    part[ j - 1 ] = part[ j ];
+                }
+                part[ part.length - 1 ] = temp;
+                list.push( createTuple( part ) );
+            }
+        
+        needed = list.filter( ( item ) => {
+            if ( item.x[ 0 ] === 0 || item.y[ 0 ] === 0 ) {
+                return false;
+            }
+            return true;
+        } );
+
+        needed =  list.map( item => {
+            const 
+                result = { 
+                    x: '',
+                    y: '',
+                    dif: -1 
+                };
+
+                item.x.forEach( item => {
+                    result.x += item 
+                } )
+
+                item.y.forEach( item => {
+                    result.y += item 
+                } )
+
+                result.x = parseInt( result.x );
+                result.y = parseInt( result.y );
+            result.dif = Math.abs( result.x - result .y );
+            return result;
+        } );
+
+        needed.sort( function ( a, b ) {
+            if ( a.dif < b.dif ) {
+                return -1;
+            }
+            return 1;
+        } );
+    return needed[ 0 ].dif || 0;
+};
+
+module.exports = function ( lines ) {
+    ( lines ).forEach( ( item, index ) => {
+        const 
+            result = init( item );
+        console.log( `#${ index + 1 }: ${ result }` );
+    } );
 };
